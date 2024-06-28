d6a62a221a659bd7cbef3c714d923a88091c73b3
Hcode00 <es-hazemahmed2025@alexu.edu.eg>
2024-06-28 04:43:58 +0300

    Improved messsages

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
 use colored::Colorize;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     loop {
         let mut guess = String::new();
-        println!("Guess a number from 1 to 10");
+        println!("Guess a number from 1 to 10 or q to quit");
         match std::io::stdin().read_line(&mut guess) {
-            Ok(_) => match guess.trim_end().parse() {
-                Ok(value) => {
-                    let secret_num = rand::thread_rng().gen_range(1..=10);
-                    if secret_num == value {
-                        println!("You Guessed the secret number.");
-                        break;
-                    } else {
-                        println!("Wrong, the secret number is {}", secret_num)
+            Ok(_) => {
+                if guess.trim() == "q" {
+                    println!("{}", "Quiting ...".red().bold());
+                    return;
+                }
+                let guess: u32 = guess.trim_end().parse().expect(stringify!(
+                    "Can't convert input to an integer!".red().bold()
+                ));
+                let secret_num: u32 = rand::thread_rng().gen_range(1..=10);
+                match guess.cmp(&secret_num) {
+                    Ordering::Equal => {
+                        println!(
+                            "{}",
+                            "You Guessed The Secret Number Correctly".green().bold()
+                        );
+                        return;
+                    }
+                    Ordering::Less => {
+                        println!("{}", "Aim a little higher".red())
+                    }
+                    Ordering::Greater => {
+                        println!("{}", "Aim a little lower".red())
                     }
                 }
-                Err(_error) => {
-                    println!("{}", "Can't convert input to an integer!".red().bold());
-                }
-            },
+            }
+
             Err(_err) => {
                 println!("{}", stringify!(err).red().bold());
-                println!("Done!")
             }
         }
     }
